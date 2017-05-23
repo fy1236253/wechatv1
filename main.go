3a04814a8fd9ad1668a783e0e7e03a033da49b7e
soleman <573239309@qq.com>
2017-05-23 23:11:34 +0800

    03

--- main.go
+++ main.go
@@ -20,7 +20,19 @@ func main() {
 	log.Println(*cfg)
 	g.ParseConfig(*cfg)
 	g.InitWxConfig()
-	log.Println(g.GetWechatConfig("gh_8ac8a8821eb8"))
+	// log.Println(g.GetWechatConfig("gh_8ac8a8821eb8"))
 	// log.Println(g.Config().Debug)
+	logTo := g.Config().Logs
+	if logTo != "stdout" {
+		f, err := os.OpenFile(logTo, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			panic(fmt.Sprintf("open logfile error"))
+		}
+		defer f.Close()
+		log.SetOutput(f)
+	}
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetPrefix(fmt.Sprintf("PID.%d ", os.Getpid()))
+	log.Println("nice")
 	http.ListenAndServe(":8089", nil)
 }
