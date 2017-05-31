d91026fa1c98f40f8fcabe3b147600f659c46393
fengya <zhangfengya@yunlaba.com>
2017-05-31 11:59:01 +0800

    测试获取菜单接口

--- src/model/wechat_model.go
+++ src/model/wechat_model.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"log"
 	"mp"
+	"mp/menu"
 	"mp/message"
 	"mp/message/request"
 	"net/http"
@@ -87,6 +88,9 @@ func ProcessWechatText(wxcfg *mp.WechatConfig, mixedMsg *message.MixedMessage) s
 	log.Println(txtContent)
 	if txtContent == "只恐夜深花睡去" {
 		SendMessageText(wxcfg.WxID, mixedMsg.FromUserName, "放下屠刀立地成佛！")
+	} else if txtContent == "佛讲缘我讲钱" {
+		SendMessageText(wxcfg.WxID, mixedMsg.FromUserName, "欢迎您管理员！")
+		menu.SearchMenu("gh_f353e8a82fe5")
 	}
 	return ""
 }
