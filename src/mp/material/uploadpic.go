a57a2349aba46c9fe658140d47821cffe7ca48f8
fengya <zhangfengya@yunlaba.com>
2017-06-01 16:32:12 +0800

    upload img

--- src/mp/material/uploadpic.go
+++ src/mp/material/uploadpic.go
@@ -2,28 +2,27 @@ package material
 
 import (
 	"g"
-	"time"
 
 	"log"
 	"os"
 
-	"github.com/toolkits/net/httplib"
+	"github.com/imroc/req"
 )
 
 // UpLodePIC 上传图片素材
 func UpLodePIC(wxid string) {
 	url := "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=" + g.GetWechatAccessToken(wxid) + "&type=image"
-	file, err := os.Open("/usr/local/src/wechatv1/public/img/u1604.png")
+	file, err := os.Open("/usr/local/src/wechatv1/public/img/duo1.jpeg")
 	log.Println(err)
 	defer file.Close()
-	req := httplib.Post(url).SetTimeout(3*time.Second, 1*time.Minute)
-	req.PostFile("media", g.Root+"/public/img/duo1.jpeg")
-	// reqs, errs := req.Post(url, req.FileUpload{
-	// 	File:      file,
-	// 	FieldName: "media",     // FieldName 是表单字段名
-	// 	FileName:  "u1604.png", // Filename 是要上传的文件的名称，我们使用它来猜测mimetype，并将其上传到服务器上
-	// })
-	// log.Println(errs)
-	resp, err := req.String()
+	// req := httplib.Post(url).SetTimeout(3*time.Second, 1*time.Minute)
+	// req.PostFile("media", g.Root+"/public/img/duo1.jpeg")
+	reqs, errs := req.Post(url, req.FileUpload{
+		File:      file,
+		FieldName: "media",     // FieldName 是表单字段名
+		FileName:  "u1604.png", // Filename 是要上传的文件的名称，我们使用它来猜测mimetype，并将其上传到服务器上
+	})
+	log.Println(errs)
+	resp := reqs.String()
 	log.Println(resp, err)
 }
