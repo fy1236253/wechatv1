0466915c07ec799e8d4a37876962e885fa2e563b
fengya <zhangfengya@yunlaba.com>
2017-06-01 15:21:52 +0800

    上传图片

--- src/mp/material/uploadpic.go
+++ src/mp/material/uploadpic.go
@@ -3,20 +3,24 @@ package material
 import (
 	"g"
 
-	"log"
+	"github.com/imroc/req"
 
-	"github.com/toolkits/net/httplib"
+	"log"
+	"os"
 )
 
 // UpLodePIC 上传图片素材
 func UpLodePIC(wxid string) {
 	url := "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=" + g.GetWechatAccessToken(wxid) + "&type=image"
-	req := httplib.Post(url)
-
-	resp, err := req.String()
-	if err == nil {
-		log.Println("[上传失败]", err)
-	}
+	file, err := os.Open("/usr/local/src/wechatv1/public/img/u1604.png")
+	log.Println(err)
+	defer file.Close()
+	reqs, errs := req.Post(url, req.FileUpload{
+		File:      file,
+		FieldName: "file",       // FieldName 是表单字段名
+		FileName:  "avatar.png", // Filename 是要上传的文件的名称，我们使用它来猜测mimetype，并将其上传到服务器上
+	})
+	log.Println(errs)
+	resp := reqs.String()
 	log.Println(resp)
-
 }
