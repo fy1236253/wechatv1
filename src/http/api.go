a689c126685f67f5b47a54abef63d7f33c170e57
fengya <zhangfengya@yunlaba.com>
2017-06-01 15:12:22 +0800

    上传图片

--- src/http/api.go
+++ src/http/api.go
@@ -56,11 +56,14 @@ func ConfigAPIRoutes() {
 		defer fW.Close()
 
 		url := "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=" + g.GetWechatAccessToken("gh_f353e8a82fe5") + "&type=image"
-		req.Post(url, req.FileUpload{
+		reqs, errs := req.Post(url, req.FileUpload{
 			File:      file,
 			FieldName: "file",       // FieldName 是表单字段名
 			FileName:  "avatar.png", // Filename 是要上传的文件的名称，我们使用它来猜测mimetype，并将其上传到服务器上
 		})
+		log.Println(errs)
+		resp := reqs.String()
+		log.Println(resp)
 		_, err = io.Copy(fW, file)
 		if err != nil {
 			fmt.Println("文件保存失败")
