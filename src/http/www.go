758ac20cb67eab0d1a28ab827fbbee62ad20e34a
fengya <zhangfengya@yunlaba.com>
2017-06-21 16:45:33 +0800

    wx

--- src/http/www.go
+++ src/http/www.go
@@ -5,7 +5,9 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"path/filepath"
+	"util"
 
 	"github.com/toolkits/file"
 )
@@ -14,6 +16,26 @@ import (
 func ConfigWebHTTP() {
 	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
 		//log.Println(openid)
+		fullurl := "http://" + r.Host + r.RequestURI
+		queryValues, err := url.ParseQuery(r.URL.RawQuery)
+		log.Println("ParseQuery", queryValues)
+		if err != nil {
+			log.Println("[ERROR] URL.RawQuery", err)
+			w.WriteHeader(400)
+			return
+		}
+		wxid := "gh_f353e8a82fe5"
+		wxcfg := g.GetWechatConfig(wxid)
+		code := queryValues.Get("code") //  摇一摇入口 code 有效
+		state := queryValues.Get("state")
+		if code == "" && state == "" {
+			addr := "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + wxcfg.AppID + "&redirect_uri=" + url.QueryEscape(fullurl) + "&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect"
+			log.Println("http.Redirect", addr)
+			http.Redirect(w, r, addr, 302)
+			return
+		}
+		openid, _ := util.GetAccessTokenFromCode(wxcfg.AppID, wxcfg.AppSecret, code)
+		log.Println(openid)
 		var f string // 模板文件路径
 		f = filepath.Join(g.Root, "/public", "index.html")
 		if !file.IsExist(f) {
@@ -30,7 +52,6 @@ func ConfigWebHTTP() {
 		if err != nil {
 			log.Println(err)
 		}
-
 		return
 	})
 
